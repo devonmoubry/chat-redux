985b7e1762c3a268977c4dba59408de031ef26f1
Devon Moubry <devon@moubry.com>
2017-03-31 13:54:41 -0500

    initial commit

---
+++ app/scripts/login-view.js
@@ -0,0 +1,25 @@
+export default function (store) {
+  console.log('The login view!');
+
+  let $html = $(`
+    <section id="loginContainer">
+      <h1>Welcome to Dev Chat!</h1>
+      <p id="instructions">Please type your username to enter Dev Chat.</p>
+      <form id="submitUsername">
+        <input type="text" required="" value="Devon" placeholder="username">
+        <input type="submit" value="Join Chat" id="submitUsername">
+      </form>
+    </section>
+  `);
+
+  $($html).find('#submitUsername').submit(function(event) {
+    event.preventDefault();
+    console.log('the login button works!');
+    store.dispatch({
+      type: "LOGIN",
+      user: $('input').val()
+    });
+  });
+
+  return $html;
+}
