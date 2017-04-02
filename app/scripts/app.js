72bc7354dc0b8affcb704507674e4cd78d306b2d
Devon Moubry <devon@moubry.com>
2017-04-02 14:39:03 -0500

    create reducer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,32 @@
 import { createStore } from 'redux';
-import loginView from './login-view.js';
-import chatView from './chat-view.js';
+// import loginView from './login-view.js';
+// import chatView from './chat-view.js';
 // import messageRender from './messages.js';
 
 
 export default function app () {
+  const initialState = {
+    items: []
+  };
+
+  const chatReducer = function( state, action ) {
+
+    if ( state === undefined ) {
+      state = initialState;
+    }
+
+    switch ( action.type ) {
+      case "TESTING":
+        console.log('It works!');
+        console.log(state);
+        return state;
+
+      default:
+        return state;
+    }
+  };
+
+  const store = createStore(chatReducer);
+  store.dispatch({ type: "TESTING" });
+
+}
